8e63f6af6dff2c1e60a5e1bc25180545e1cbed78
Volen Nakov <volen.nakov@gmail.com>
2025-03-30 02:02:41 +0200

    fix: Remove unremovable cousin of his

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,7 +1,4 @@
-from pydantic_settings import BaseSettings
-from functools import lru_cache
-
-class Settings(BaseSettings):
+class Settings:
     PROJECT_NAME: str = "NeuroPixel"
     VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
@@ -12,11 +9,6 @@ class Settings(BaseSettings):
     
     # ML Model settings
     MODEL_PATH: str = "models/ml_model"  # Path to your ML model
-    
-    class Config:
-        case_sensitive = True
-        env_file = ".env"
 
-@lru_cache()
 def get_settings():
     return Settings() 
